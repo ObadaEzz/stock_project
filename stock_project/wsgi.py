cc7caccd728fd8a57f1257f2fa271d176b3fe75f
ObadaEzz <102419199+ObadaEzz@users.noreply.github.com>
2024-10-26 00:26:53 +0300

    Update wsgi.py

--- stock_project/wsgi.py
+++ stock_project/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for stock_project project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
+import sys
 import os
+from pathlib import Path
 
+# أضف المسار الكامل لمجلد المشروع
+project_path = '/home/ObadaEz/stock_project/'
+if project_path not in sys.path:
+    sys.path.append(project_path)
+
+# إعداد Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'stock_project.settings'
+
+# تحميل تطبيق WSGI الخاص بـ Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_project.settings')
-
 application = get_wsgi_application()
+
